c2cd57aa39acadacbd2cb51730ede567412c44f8
sajin-ss <sajin.kun.7@gmail.com>
2017-01-29 20:06:36 +0530

    doing DB

---
+++ app/controllers/tasks/new.js
@@ -0,0 +1,31 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	actions: {
+		addTask : function(){
+			var Title = this.get('title');
+			var Status = this.get('status');
+
+
+			var taskSet = this.store.createRecord('mytasks', {
+				title: Title,
+				status: Status
+			});
+
+			console.log('Added');
+
+			var tasks = this.store.findAll('mytasks');
+			console.log(tasks);
+
+
+			this.setProperties({
+				title : '',
+				status : ''
+			});
+
+			taskSet.save();
+
+
+		}
+	}
+});
